3967fd3070c2c3cf18d18838feeedbdc51b2e066
talha <talhazencirkiran@marun.edu.tr>
2025-12-15 20:51:19 +0300

    i break the values for some reason. I'll change heapify method.

--- lab-4/problem1.cpp
+++ lab-4/problem1.cpp
@@ -12,27 +12,62 @@ class MinHeap {
 private:
     vector<int> heap;
 
-    int parent(int i) { return -1; }
-    int left(int i) { return -1; }
-    int right(int i) { return -1; }
+    int parent(int i) { return (i-1)/2; }
+    int left(int i) { return 2*i+1; }
+    int right(int i) { return 2*i+2; }
 
     void heapify(int i) {
-        // Implement this
+        // i == 0, assumes min element removed and replaced with the last element
+        if (i == 0) { // perculate down, i for tracking replaced last node (after deleteMin)
+            int idx_max = heap.size() - 1;
+            int temp;
+            while (i < idx_max && (heap[i] > left(i) || heap[i] > right(i))) {
+                if (right(i) < left(i)) {
+                    temp = heap[i];
+                    heap[right(i)] = temp;
+                    heap[i] = right(i);
+                    i = right(i);
+                }
+                else {
+                    temp = heap[i];
+                    heap[left(i)] = temp;
+                    heap[i] = left(i);
+                    i = left(i);
+                }
+
+            }
+        }
+        else {
+            while (i > 0 && heap[parent(i)] > heap[i]) // i is not 0 (not root), i for tracking new value
+            {
+                int temp = heap[i];
+                heap[i] = heap[parent(i)]; // changing inserted and parnt if new<parent
+                i = parent(i); // i updated with its parent's idx due to replacement
+            }
+        }
     }
 
 public:
     void insert(int val) {
         // Implement this
+        heap.push_back(val);
+        int i = heap.size() - 1; // i for keeping track of where new value is
+        heapify(i);
     }
 
     int extractMin() {
         // Implement this
-        return -1;
+        int min = heap[0];
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapify(0);
+
+        return min;
     }
 
     int getMin() {
         // Implement this
-        return -1;
+        return heap[0];
     }
 
     bool isEmpty() {
