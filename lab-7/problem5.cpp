0f51bbb1f8b0636340ada9e081324b967794595e
talha <talhazencirkiran@marun.edu.tr>
2026-01-06 23:29:49 +0300

    Problem 5 solved.

--- lab-7/problem5.cpp
+++ lab-7/problem5.cpp
@@ -45,6 +45,20 @@ public:
         vector<int> dist(numCities, INT_MAX);
         
         // TODO: Implement Dijkstra's Algorithm
+        priority_queue<pair<int,int>, vector<pair<int,int>>,
+        greater<pair<int,int>>> pq;
+        dist[source] = 0;
+        pq.push({0, source}); // (distance, vertex)
+        while (!pq.empty()) {
+            auto [d, u] = pq.top(); pq.pop();
+            if (d > dist[u]) continue; // Skip outdated entries
+            for (auto [v, w] : adjList[u]) {
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
         
         return dist;  // placeholder
     }
